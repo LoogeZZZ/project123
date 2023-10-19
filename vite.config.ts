6bc9c8de39df995786d5979e884c9445d2ee1d78
Pavel Bezusov <looge.dark@mail.ru>
2023-10-19 22:32:25 +0300

    123

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/project123",
   server: { watch: { awaitWriteFinish: true } },
   plugins: [react()],
 });
